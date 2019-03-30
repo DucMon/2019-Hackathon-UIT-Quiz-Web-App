21c82231abcbf8c1a98c08664bc4565368b897eb
ducnguyn <stephenmon8991@gmail.com>
2019-03-30 23:24:40 +0700

    fix bug

--- server.js
+++ server.js
@@ -3,8 +3,6 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/views'));
-
-app.use(express.static(__dirname + '/homepage'));
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 app.set('views', './views');
@@ -19,6 +17,15 @@ app.get('/detailquestion', function(req, res) {
 });
 app.get('/quizset', function(req, res) {
     res.render('quizSet');
+});
+app.get('/admin', function(req, res) {
+    res.render('quizAdmin');
+});
+app.get('/signin', function(req, res) {
+    res.render('signin');
+});
+app.get('/signup', function(req, res) {
+    res.render('signup');
 });
 
 
